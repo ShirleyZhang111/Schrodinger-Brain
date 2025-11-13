84c88db321d39e46832fb96c375fcc75019c6d4d
Jiangnan Zhang <113335224+ShirleyZhang111@users.noreply.github.com>
2025-11-13 21:58:23 +0800

    Update analysis_stimulus_envoked_SC_nonlinear.m

--- complex_brain/analysis_stimulus_envoked_SC_nonlinear.m
+++ complex_brain/analysis_stimulus_envoked_SC_nonlinear.m
@@ -22,18 +22,22 @@ S_construct = S_construct.';
 % time: response time; 
 S0 = abs(S_construct);
 for id_cortex = 1:node_items
-    if ismember(id_cortex,region) ==0
+    if ismember(id_cortex,region) == 0
         [pks1,locs1] = findpeaks(S0(id_cortex,:));
         if(isempty(locs1)==0)
             time_complex(id_cortex,1) = locs1(1);
+        else
+            time_complex(id_cortex,1) = 1000;
         end
     end
 end
-% value: response amplitude; 
 time_complex(region,1) = 0;
+
+% value: response amplitude; 
+value_complex = zeros(node_items,1);
 for id_cortex = 1:node_items
     if(ismember(id_cortex,region) == 0)
-        value_complex(id_cortex,1) = S0(id_cortex,time_complex(id_cortex)+1);
+        value_complex(id_cortex,1) = S0(id_cortex,time_complex(id_cortex));
     end
 end
 value_complex(region,1) = 10;
@@ -42,16 +46,17 @@ label = sortrows(cell2mat(LabelID(:,[2,5])),1);
 [~,G0,~,~,~] = calc_network_measures(SC,label);
 coup_strength = G0(:,region);  % Communicability of specific region 
 
+
 % Response amplitude - Communicability
 Amp_com = [coup_strength,value_complex];
 Amp_com = sortrows(Amp_com,1);
 figure;
-plot(Amp_com(:,1),Amp_com(:,2),'LineWidth',0.7,'Color',[150  59  121]/255);hold on;
+loglog(Amp_com(:,1),Amp_com(:,2),'LineWidth',0.7,'Color',[150  59  121]/255);hold on;
 xlim([min(coup_strength),max(coup_strength)])
 
 % Response time - Communicability
-Time_com = [coup_strength,time_complex];
-Time_com = sortrows(Time_com,1)/100;
+Time_com = [coup_strength,time_complex/100];
+Time_com = sortrows(Time_com,1);
 figure;
-plot(Time_com(:,1),Time_com(:,2),'LineWidth',1,'Color',[150  59  121]/255);hold on;
+semilogx(Time_com(:,1),Time_com(:,2),'LineWidth',1,'Color',[150  59  121]/255);hold on;
 xlim([min(coup_strength),max(coup_strength)])
