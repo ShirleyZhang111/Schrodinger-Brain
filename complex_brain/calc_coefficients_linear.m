e262d6594766537e7b8c862cc656e3292949122a
Jiangnan Zhang <113335224+ShirleyZhang111@users.noreply.github.com>
2025-11-13 17:57:51 +0800

    Update calc_coefficients_linear.m

--- complex_brain/calc_coefficients_linear.m
+++ complex_brain/calc_coefficients_linear.m
@@ -35,14 +35,16 @@ elseif strcmp(field, 'real') == 1
 end
 
 %  linear model with orthgonality  
+T0 = length(T);
 A = data(:,1:end-1);
 B = data(:,2:end);
 [m,~] = size(A);
 [U_r,S_r,V_r] = svd([A,B],'econ');
-V1 = V_r(1:T-1,:);
-V2 = V_r(T:2*(T-2),:);
+V1 = V_r(1:T0-1,:);
+V2 = V_r(T0:2*T0-2,:);
 [U0,~,V0] = svd(S_r*V2'*V1*S_r');
-Q = eye(m)-U_r*(eye(2*(T-2))-U0*V0')*U_r';
+Q = eye(m)-U_r*(eye(2*(T0-2))-U0*V0')*U_r';
 [G,D] = eig(Q);
 lambda = diag(D);
+
 save([save_path file_name(4:end-4) '_linear_fitting_' field '.mat'], 'Q','G','lambda');
