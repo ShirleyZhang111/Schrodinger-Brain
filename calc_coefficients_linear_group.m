17f6d94f78c93a90e9103e9891b102b30ff5ad4f
Jiangnan Zhang <113335224+ShirleyZhang111@users.noreply.github.com>
2026-04-19 21:17:04 +0800

    Update calc_coefficients_linear_group.m

--- calc_coefficients_linear_group.m
+++ calc_coefficients_linear_group.m
@@ -1,12 +1,13 @@
 function [Q,G,lambda] = calc_coefficients_linear_group(inCfg,TC_group)
 % Group-level linear model with orthogonal constraint
-%     min_{Q*Q = I} sum_s sum_j norm(Q reshape(z(:,j,s),[],1) - reshape(z(:,j+1,s),[],1))^2
+%     complex: min_{Q*Q = I} sum_s sum_j norm(Q reshape(z(:,j,s),[],1) - reshape(z(:,j+1,s),[],1))^2
+%     real: min sum_s sum_j norm(Q reshape(z(:,j,s),[],1) - reshape(z(:,j+1,s),[],1))^2
 % Input: 
 %     z : space * time * subjects
 %     T : time points for fitting
 %     field : complex field or real field
 % Output:
-%     Q : the unitary transfer matrix
+%     Q : the group-level transfer matrix
 %     G : the eigenvectors of Q 
 %     lambda : the eigenvalues of Q
 
@@ -24,28 +25,40 @@ if isfield(inCfg,'idx');idx = inCfg.idx;else;idx = 1;end % space, number of node
 % items = items(3:end);
 
 W = zeros(idx,idx);
-for k = 1:item_number
-    % item = items(k).name;
-    % data = load(strcat(data_path, '/', item));
-    % TC = data.TC;
-    TC = TC_group{1,k};
-    if strcmp(field, 'complex') == 1
+W1 = zeros(idx,idx); 
+W2 = zeros(idx,idx);
+if strcmp(field, 'complex') == 1
+% Complex-valued linear model
+    for k = 1:item_number
+        % item = items(k).name;
+        % data = load(strcat(data_path, '/', item));
+        % TC = data.TC;
+        TC = TC_group{1,k};
         % Complex-valued linear model
         hilbert_data = double(hilbert(zscore(TC'))).'; clearvars TC
-        data = hilbert_data(:,T); clearvars hilbert_data
-    elseif strcmp(field, 'real') == 1
-        % Real-valued linear model
-        zscore_data = double(zscore(TC')).';clearvars TC
-        data = zscore_data(:,T); clearvars zscore_data
-    end
-     
-    A = data(:,1:end-1);
-    B = data(:,2:end);
-    W = W + B*A';
+        data = hilbert_data(:,T); clearvars hilbert_data    
+        A = data(:,1:end-1);
+        B = data(:,2:end);
+        W = W + B*A';
+    end    
+    [U,~,V] = svd(W);
+    Q = U*V';
+    [G,D] = eig(Q);
+    lambda = diag(D);
+elseif strcmp(field, 'real') == 1
+% Real-valued linear model
+    for k = 1:item_number
+        % item = items(k).name;
+        % data = load(strcat(data_path, '/', item));
+        % TC = data.TC;
+        TC = TC_group{1,k};
+        zscore_data = double(zscore(TC')).'; clearvars TC
+        data = zscore_data(:,T); clearvars zscore_data    
+        A = data(:,1:end-1);
+        B = data(:,2:end);
+        W1 = W1 + A*A'; W2 = W2 + B*A';
+    end  
+    Q = W2 / W1;
 end
-
-[U,~,V] = svd(W);
-Q = U*V';
-[G,D] = eig(Q);
-lambda = diag(D);
+    
 % save([save_path 'Group_rfMRI_resting_Q_G_lambda_' field '.mat'], 'Q','G','lambda');
