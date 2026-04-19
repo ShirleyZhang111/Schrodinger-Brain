7b239d9b34c10b5eb6da677c4200d2be7e5c2da3
Jiangnan Zhang <113335224+ShirleyZhang111@users.noreply.github.com>
2026-04-19 21:09:30 +0800

    Update calc_coefficients_linear.m

--- calc_coefficients_linear.m
+++ calc_coefficients_linear.m
@@ -1,6 +1,7 @@
 function [Q,G,lambda] = calc_coefficients_linear(inCfg)
 % Individual-level linear model with orthogonal constraint
-%     min_{Q*Q = I} sum_j norm(Qz(:,j) - z(:,j+1))^2
+%     complex-valued : min_{Q*Q = I} sum_j norm(Qz(:,j) - z(:,j+1))^2
+%     real-valued : min sum_j norm(Qz(:,j) - z(:,j+1))^2
 % Input: 
 %     z : space * time
 %     T : time points for fitting
@@ -21,22 +22,27 @@ if strcmp(field, 'complex') == 1
     % Complex-valued data
     hilbert_data = double(hilbert(zscore(TC'))).';
     data = hilbert_data(:,T); clearvars hilbert_data
+    %  Compute model parameters with orthgonality  
+    T0 = length(T);
+    A = data(:,1:end-1);
+    B = data(:,2:end);
+    [m,~] = size(A);
+    [U_r,S_r,V_r] = svd([A,B],'econ');
+    V1 = V_r(1:T0-1,:);
+    V2 = V_r(T0:2*T0-2,:);
+    [U0,~,V0] = svd(S_r*V2'*V1*S_r');
+    Q = eye(m)-U_r*(eye(2*T0-2)-U0*V0')*U_r';
+    [G,D] = eig(Q);
+    lambda = diag(D);
+    % save([save_path file_name(4:end-4) '_linear_fitting_' field '.mat'], 'Q','G','lambda');
 elseif strcmp(field, 'real') == 1
     % Real-valued data
     zscore_data = double(zscore(TC')).';
     data = zscore_data(:,T); clearvars zscore_data
+    T0 = length(T);
+    A = data(:,1:end-1);
+    B = data(:,2:end);
+    Q = B/A;
+    [G,D] = eig(Q);
+    lambda = diag(D);
 end
-
-%  Compute model parameters with orthgonality  
-T0 = length(T);
-A = data(:,1:end-1);
-B = data(:,2:end);
-[m,~] = size(A);
-[U_r,S_r,V_r] = svd([A,B],'econ');
-V1 = V_r(1:T0-1,:);
-V2 = V_r(T0:2*T0-2,:);
-[U0,~,V0] = svd(S_r*V2'*V1*S_r');
-Q = eye(m)-U_r*(eye(2*T0-2)-U0*V0')*U_r';
-[G,D] = eig(Q);
-lambda = diag(D);
-% save([save_path file_name(4:end-4) '_linear_fitting_' field '.mat'], 'Q','G','lambda');
