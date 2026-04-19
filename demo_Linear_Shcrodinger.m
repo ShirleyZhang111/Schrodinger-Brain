b9e462449b0f314ae04f7b87b0cc0460f9321557
Jiangnan Zhang <113335224+ShirleyZhang111@users.noreply.github.com>
2026-04-19 21:06:05 +0800

    Update demo_Linear_Shcrodinger.m

--- demo_Linear_Shcrodinger.m
+++ demo_Linear_Shcrodinger.m
@@ -1,25 +1,25 @@
 %% Linear Schrodinger-like model
-% This code implements the linear Schrodinger-like model for fMRI data analysis
+% This code implements the linear Schrodinger-like model for fMRI data analysis (prediction for voxel-level data)
 
 % load voxel-level rs-fMRI data
 load('./Data/TC_HCP_9982.mat');
 
 % Set model configuration parameters
 field = 'complex';
-inCfg = struct('field',field, 'T', 101:400, 'TC',TC); % Configuration structure: use time points 101-400 to compute model parameters
+inCfg = struct('field',field, 'T', 101:500, 'TC',TC); % Configuration structure: use time points 101-400 to compute model parameters
 
 % Calculate parameters for linear Schrodinger-like model
 [Q,G,lambda] = calc_coefficients_linear(inCfg);
 
 % Time series prediction
-% Prediction length: 25 TRs, Initial state: 401st TR
+% Prediction length: 20 TRs, Initial state: 501st TR
 cfg = struct('field',field, 'T', 101:1000, 'Q',Q,...
-    'Initial_time', 401-101, 'signal_length',25,'TC',TC);
+    'Initial_time', 501-101, 'signal_length',20,'TC',TC);
 
 % Execute prediction and get results
 [data_predicted,data_obeserved,correlation] = calc_prediction(cfg);
 
-% Analyze 25 TR prediction results
+% Analyze 20 TR prediction results
 figure; 
 plot(data_obeserved(1,:)); hold on;
 plot(data_predicted(1,:)); hold on;
