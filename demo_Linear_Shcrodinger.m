cc77d31d10134b701de5c4dbcd67e2859d2838f8
Jiangnan Zhang <113335224+ShirleyZhang111@users.noreply.github.com>
2026-04-19 21:28:41 +0800

    Update demo_Linear_Shcrodinger.m

--- demo_Linear_Shcrodinger.m
+++ demo_Linear_Shcrodinger.m
@@ -12,14 +12,14 @@ inCfg = struct('field',field, 'T', 101:500, 'TC',TC); % Configuration structure:
 [Q,G,lambda] = calc_coefficients_linear(inCfg);
 
 % Time series prediction
-% Prediction length: 20 TRs, Initial state: 501st TR
+% Prediction length: 10 TRs, Initial state: 501st TR
 cfg = struct('field',field, 'T', 101:1000, 'Q',Q,...
-    'Initial_time', 501-101, 'signal_length',20,'TC',TC); % initial 501, for time points 101-500, initial = 501-101
+    'Initial_time', 501-101, 'signal_length',10,'TC',TC); % initial 501, for time points 101-500, initial = 501-101
 
 % Execute prediction and get results
 [data_predicted,data_obeserved,correlation] = calc_prediction(cfg);
 
-% Analyze 20 TR prediction results
+% Analyze 10 TR prediction results
 figure; 
 plot(data_obeserved(1,:)); hold on;
 plot(data_predicted(1,:)); hold on;
